3708455460d8158fb6496b53512e27df7d640849
Daniel Leal <daniel_leal25@gmail.com>
2021-10-21 15:29:54 -0400

    prisma e configs

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,17 @@
+import "dotenv/config"
 import express from "express";
 
 const app = express();
+
+app.use(express.json());
+
+app.get("/github", (req, res) => {
+  res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`);
+});
+
+app.get("/signin/callback", (req, res) => {
+  const { code } = req.query;
+  return res.json(code);
+})
 
 app.listen(4000, () => console.log(`🚀🧑‍🚀server is running on PORT 4000`));
